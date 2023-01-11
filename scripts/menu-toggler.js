ecd315277e668df9a6af17852e77572d238aea64
Iqbal Zayyan <iqbalzayyanabiyyu@gmail.com>
2023-01-11 13:22:02 +0700

    update navbar

--- scripts/menu-toggler.js
+++ scripts/menu-toggler.js
@@ -3,55 +3,48 @@ import { addClass, removeClass } from "./utils-class.js";
 const menuTogglerId = document.getElementById("menu-toggler");
 const menu = document.getElementById("menu");
 const navBrand = document.getElementById("nav-brand");
-
+const menuId = document.getElementById("menu");
 
 menuTogglerId.addEventListener("click", function() {
-    const menuId = document.getElementById("menu");
-    const trigger = document.getElementById("trigger");
+  const nav = document.getElementById("nav");
+  const navbar = document.getElementById("navbar");
 
-    if (trigger.className.indexOf("relative") > -1) {
-      addClass(trigger, "hidden");
-      removeClass(trigger, "hidden relative");
-    } else {
-      removeClass(trigger, "hidden");
-      addClass(trigger, "relative");
-    }
+  if (menuId.className.indexOf("hidden") > -1) {
+      addClass(menuTogglerId, "z-50");
+      addClass(menuId, "flex py-20 absolute inset-0 bg-black1 h-screen");
+      addClass(nav, "hidden");
+      addClass(navbar, "pt-8");
+      removeClass(menuId, "hidden");
+      removeClass(nav, "relative");
+  } else {
+    removeClass(menuTogglerId, "z-50");
+    addClass(menuId, "hidden");
+    addClass(nav, "relative");
+    removeClass(menuId, "flex py-20 absolute inset-0 bg-black1 h-screen");
+    removeClass(nav, "hidden");
+    removeClass(navbar, "pt-8");
+  }
+})
 
-    if (menuId.className.indexOf("hidden") > -1) {
-        addClass(menuTogglerId, "z-50");
-        // removeClass(menuTogglerId, "relative");
-        addClass(menuId, "flex absolute inset-0 bg-black1 h-screen");
-        removeClass(menuId, "hidden");
-    } else {
-      removeClass(menuTogglerId, "z-50");
-      // addClass(menuTogglerId, "relative");
-      addClass(menuId, "hidden");
-      removeClass(menuId, "flex absolute inset-0 bg-black1 h-screen");
-    }
+
+function navToggler() {
+  if (menuId.className.indexOf("flex") > -1) {
+    removeClass(menuId, "flex py-20 absolute inset-0 bg-black1 h-screen");
+    removeClass(menuTogglerId, "z-50");
+    removeClass(nav, "hidden");
+    removeClass(navbar, "pt-8");
+    addClass(menuId, "hidden");
+    addClass(nav, "relative");
+  }
+  return navToggler();
+}
+
+
+navBrand.addEventListener("click", function() {
+  navToggler();
 })
 
 
 menu.addEventListener("click", function() {
-  const menuId = document.getElementById("menu");
-  
-  if (menuId.className.indexOf("flex") > -1) {
-    removeClass(menuTogglerId, "z-50");
-    // addClass(menuTogglerId, "relative");
-    addClass(menuId, "hidden");
-    removeClass(menuId, "flex absolute inset-0 bg-black1 h-screen");
-  } 
-
+  navToggler();
 })
-
-
-navBrand.addEventListener("click", function() {
-  const menuId = document.getElementById("menu");
-  
-  if (menuId.className.indexOf("flex") > -1) {
-    removeClass(menuTogglerId, "z-50");
-    // addClass(menuTogglerId, "relative");
-    addClass(menuId, "hidden");
-    removeClass(menuId, "flex absolute inset-0 bg-black1 h-screen");
-  } 
-})
-
